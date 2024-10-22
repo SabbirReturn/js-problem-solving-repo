34d5a18a3886ae83016a3e6838743d8fa5c26895
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-23 01:45:39 +0600

    task2

---
+++ simple_operation_task/task2.js
@@ -0,0 +1,9 @@
+var mathematicsMark = 75.25; 
+var biologyMark =65;
+var chemistryMark =80;
+var physicsMark =35.45;
+var banglaMark =99.50;
+var totalSubject = 5;
+var totalMark = mathematicsMark + biologyMark + chemistryMark + physicsMark + banglaMark;
+var averageMark = totalMark / totalSubject;
+console.log(parseFloat(averageMark.toFixed(2)));
