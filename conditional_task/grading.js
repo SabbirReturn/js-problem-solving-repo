2e5de0c9114d15212609f49f797571282e0218d7
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-25 04:22:26 +0600

    grading

---
+++ conditional_task/grading.js
@@ -0,0 +1,17 @@
+const mark = 8;
+if(mark<=100 && mark>=90){
+    console.log('A')
+}
+
+else if(mark<=89 && mark>= 80){
+    console.log('B')
+}
+else if(mark<=79 && mark>=70){
+    console.log('C')
+}
+else if(mark<=69 && mark>=60){
+    console.log('D')
+}
+else{
+    console.log('F')
+}
