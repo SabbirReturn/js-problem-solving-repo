11c4eae8b2b2dcd91e6ac1ce5d16d731a0d38fa1
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-25 04:34:55 +0600

    nasted

---
+++ conditional_task/nasted_friend.js
@@ -0,0 +1,19 @@
+const mark =62;
+if( mark>80){
+    console.log('Go for a lunch')
+}
+else{
+    if(mark<80 && mark>=60){
+        console.log('Good luck next time')
+    }
+    else{
+        if(mark<60 && mark>= 40){
+            console.log('Message unseen')
+        }
+        else{
+            if(mark< 80){
+                console.log("go to home and sleep and act sad")
+            }
+        }
+    }
+}
