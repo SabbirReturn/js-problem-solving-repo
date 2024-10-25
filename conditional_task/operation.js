359495414fff35157035534e6b60a166454f8121
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-25 15:03:08 +0600

    ternary

---
+++ conditional_task/operation.js
@@ -0,0 +1,13 @@
+const num1 = 10;
+const num2 = 2;
+if( num1 > num2){
+    const result = num1 * 2;
+    console.log(result);
+}
+else{
+    const result = num1 + num2;
+    console.log(result);
+}
+
+//  simple ternary
+// result = num1 > num2 ? console.log(num1 *2) : console.log(num1 + num2)
