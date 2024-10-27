422950777feb44bc38ea9f7cdc702344c5447e70
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-27 06:39:44 +0600

    add_remove

---
+++ Array/add_remove.js
@@ -0,0 +1,8 @@
+const touristPlaceName = ['Bandarban','Sundarbon','Cox Bazar'];
+const addName = touristPlaceName.push('Ranggamati');
+console.log(touristPlaceName);
+touristPlaceName.push('Sentmartin','Kushtia');
+// touristPlaceName.push('Kushtia');
+console.log(touristPlaceName);
+touristPlaceName.pop();
+console.log(touristPlaceName);
