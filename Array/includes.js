4582a0a7e4ab6fcff9cec1f4e2eb993a46ffd8d4
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-27 06:44:41 +0600

    includes

---
+++ Array/includes.js
@@ -0,0 +1,9 @@
+const booksName = ['Physics','Math','biology','ICT'];
+const verifyName = booksName.includes('Biology');
+console.log(verifyName);
+if(booksName.includes('Biology')){
+    console.log('I will got A+')
+}
+else{
+    console.log('Obosta valo na');
+}
