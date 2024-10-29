8c850825d539fe0abd498ef68c9bc9027a8a23ce
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-30 03:49:27 +0600

    odd-even-while

---
+++ loop/odd-even-while.js
@@ -0,0 +1,20 @@
+// Find all the odd numbers from 61 to 100.
+
+let n = 61;
+while(n<=100){
+    if(n % 2 ===1){
+        console.log('Odd number',n);
+    }
+    n++
+}
+
+
+// Find all the even numbers from 78 to 98.
+
+let i = 78;
+while(i<98){
+    if(i % 2 ===0){
+        console.log('Even number',i);
+    }
+    i++
+}
