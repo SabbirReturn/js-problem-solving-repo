fe4fc6bd363c443b7bb179742a1926ea48bc2a21
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-10-30 03:55:28 +0600

    sum-odd-even

---
+++ loop/sum-odd-even-while.js
@@ -0,0 +1,25 @@
+// Display sum of all the odd numbers from 81 to 131.
+
+// let i = 81;
+// let sum = 0;
+// while(i<=131){
+//     if(i % 2 !==0){
+//         console.log(i);
+//         sum = sum + i;
+//         console.log('Total',sum)
+//     }
+//     i++
+// }
+
+// Display sum of all the even numbers from 206 to 311.
+
+let n = 206;
+let sum = 0;
+while(n<=311){
+    if(n % 2 ===0){
+        console.log(n);
+        sum = sum + n;
+        console.log('Total', sum);
+    }
+    n++
+}
