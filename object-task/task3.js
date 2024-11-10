8aa3d3701a6eb473cbe00a5b7022c7874b3d7378
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-11 02:07:24 +0600

    access-marl

---
+++ object-task/task3.js
@@ -0,0 +1,15 @@
+// Display the physics marks as output.
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+console.log(student);
+// console.log(student.physics.marks);
+let mark = student.physics.marks;
+    mark = 40
+console.log(mark);
