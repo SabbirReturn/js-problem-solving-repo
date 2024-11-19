ac8a7bef2170495ecb74de74caccccc4f77f4181
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-20 00:55:08 +0600

    task3

---
+++ string/task3.js
@@ -0,0 +1,8 @@
+// Check whether a string contains all the vowels a, e, i, o, u
+const sentence = 'I em a sabbir Hosun';
+// let vowel = ['a','e','i','o','u'];
+// let single = sentence.split('')
+// console.log(single);
+for(let word of sentence){
+    console.log(word);
+}
