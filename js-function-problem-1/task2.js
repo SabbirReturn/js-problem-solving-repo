1ddc5081c212be749e4af52c5c3141a534f3a2f6
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-12-11 16:58:10 +0600

    try to another way

--- js-function-problem-1/task2.js
+++ js-function-problem-1/task2.js
@@ -1,11 +1,25 @@
-function countOccurrences(arr, target) {
-    return arr.filter(num => num === target).length;
+// function countOccurrences(arr, target) {
+//     return arr.filter(num => num === target).length;
+// }
+
+// // Sample input
+// let numbers = [5, 6, 11, 12, 98, 5];
+// let target = 5;
+
+// // Find occurrences
+// let result = countOccurrences(numbers, target);
+// console.log(result); // Outputs: 2
+
+
+function countOccurrences(num1, num2){
+    let unique = [];
+    let countNumber = [];
+    for(let number of num1){
+        unique.push(number);
+        if(unique.includes(num2)===false){
+            countNumber.push(unique);
+            console.log(countNumber);
+        }
+    }
 }
-
-// Sample input
-let numbers = [5, 6, 11, 12, 98, 5];
-let target = 5;
-
-// Find occurrences
-let result = countOccurrences(numbers, target);
-console.log(result); // Outputs: 2
+countOccurrences(([5,6,11,12,98, 5]),5)
