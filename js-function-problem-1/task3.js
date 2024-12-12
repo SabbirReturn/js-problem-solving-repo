4b69b76c9f6ca542965789fee2e8877727c2fa3d
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-12-13 03:04:01 +0600

    task3

--- js-function-problem-1/task3.js
+++ js-function-problem-1/task3.js
@@ -1,16 +1,33 @@
 // Write a function to count the number of vowels in a string.
-function countVowels(str) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i].toLowerCase();
-        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
-            count++;
+// function countVowels(str) {
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         let char = str[i].toLowerCase();
+//         if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// // Example usage
+// let input = "Hello World";
+// let result = countVowels(input);
+// console.log(result); // Outputs: 3
+
+function countVowels(str){
+    let count = [];
+    for(let i=0; i < str.length; i++){
+        let char = str[i];
+        // console.log(char);
+        
+        if(char==='a' || char === 'e' || char ==='i' || char ==='o' || char==='u'){
+            count.push(char);
         }
     }
-    return count;
+    let len = count.length;
+    return len;
 }
-
-// Example usage
-let input = "Hello World";
+let input = 'I am a student';
 let result = countVowels(input);
-console.log(result); // Outputs: 3
+console.log(result);
