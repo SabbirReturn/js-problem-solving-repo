a02cd652ea4b017b19600701ce8a66a1e5d67975
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-12-20 01:53:56 +0600

    get max number

---
+++ js-function-problem-2/max.js
@@ -0,0 +1,19 @@
+let sabbir = 80;
+let hasan = 98;
+if(sabbir > hasan){
+    // console.log('Sabbir Gate the stobary');
+}
+else{
+    // console.log('Hasan get the stobary');
+}
+
+function getBigMark(num1, num2){
+    if(num1>num2){
+        return true
+    }
+    else{
+        return false
+    }
+}
+let marks = getBigMark(564,65);
+console.log(marks)
