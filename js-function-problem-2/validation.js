8cd2b0a71e8872d0513d3b1e901eec060c5e7e2f
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-08 01:39:49 +0600

    validation

--- js-function-problem-2/validation.js
+++ js-function-problem-2/validation.js
@@ -12,17 +12,40 @@
 // console.log(result);
 
 
-function fullName ( first, second){
-    if(typeof first !== 'string'){
-        return 'First name should be a string'
+// function fullName ( first, second){
+//     if(typeof first !== 'string'){
+//         return 'First name should be a string'
+//     }
+//     else if (typeof second !== 'string'){
+//         return 'Second name should be a string'
+//     }
+//     let result = first + ' ' +second;
+//     return result;
+// }
+// let firstName = 'Sabbir'
+// let lastName = 7
+// let full = fullName(firstName,lastName)
+// console.log(full)
+
+// function getPrice(product){
+//     if(typeof product !=='object'){
+//         return 'Give me a object'
+//     }
+//     let cost = product.price;
+//     return cost;
+// }
+// // let price = getPrice({name:'Samsung',price:5});
+// let price = getPrice(5)
+// console.log(price)
+
+function getSecond(numbers){
+    if(Array.isArray(numbers)==false){
+        return 'Give me a Array'
     }
-    else if (typeof second !== 'string'){
-        return 'Second name should be a string'
-    }
-    let result = first + ' ' +second;
-    return result;
+    let number = numbers[1];
+    return number;
 }
-let firstName = 'Sabbir'
-let lastName = 7
-let full = fullName(firstName,lastName)
-console.log(full)
+
+// let number = getSecond([1,2,3,4,5,6])
+let number = getSecond(4)
+console.log(number);
